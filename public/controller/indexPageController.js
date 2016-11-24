dd3c6aa60f90c1bd063a2300c8343f64ad24bfb3
Shubham SC <20592@ITCINFOTECH.COM>
2016-11-24 19:39:40 +0530

    Signed-off-by: Shubham SC <20592@ITCINFOTECH.COM> added with functionality

--- public/controller/indexPageController.js
+++ public/controller/indexPageController.js
@@ -1,6 +1,75 @@
 (function(){
 	var moduleForIndexPage = angular.module('mainModule');	
-	moduleForIndexPage.controller('indexPageController',['$scope','indexPageService',function($scope,indexPageService){		
-		console.log(indexPageService.getCardColors());
+	moduleForIndexPage.controller('indexPageController',['$scope','indexPageService',function($scope,indexPageService){				
+		/*############## variable declaration ######### */
+		var actualCardArray = indexPageService.getCardColors();
+		var ref_identifier = 0;
+		$scope.achievedScore = 0;
+		$scope.remainingScore = 50;
+		$scope.cardData = shuffle(actualCardArray);
+
+		console.log($scope.cardData);
+		/*############## function declaration ######### */
+		    /**********this function randomizes the object coming from the backend **********/
+			function shuffle(array) {
+			  var copy = [], n = array.length, i;		  
+			  while (n) {		  
+			    i = Math.floor(Math.random() * array.length);		  
+			    if (i in array) {
+			      copy.push(array[i]);
+			      delete array[i];
+			      n--;
+			    }
+			  }
+			  return copy;
+			};
+
+			function checkForFirstClick(referenceData){
+				if(referenceData==0){
+					return true;
+				}
+				return false;
+			}
+			function validRemainingScore(score){
+				if(score>15){
+					return true;
+				}
+				return false;
+			}
+			function setTheReferenceData(comingData){
+				ref_identifier = comingData;
+				console.log("reference set to " + ref_identifier);
+			}
+			function dataMatched(secondClickData){
+				if(ref_identifier == secondClickData){
+					return true;
+				}
+				return false;
+			}
+			function setReferenceToDefault(){
+				ref_identifier = 0;
+				return;
+			}
+			$scope.checkForTheMatch = function(identifier){
+				console.log(identifier.data);
+				if(checkForFirstClick(ref_identifier)){
+					setTheReferenceData(identifier.data.card_identifier);					
+					console.log("first time");
+					return null;
+				}
+				else{
+					console.log("second time");
+					if(dataMatched(identifier.data.card_identifier)){
+						console.log("matched");
+						$scope.achievedScore =$scope.achievedScore+20;
+						setReferenceToDefault();
+					}
+					else{
+						$scope.remainingScore = $scope.remainingScore -20;
+						setReferenceToDefault();
+					}
+				}
+			}
+
 	}]);
 })();
--- public/services/indexPageService.js
+++ public/services/indexPageService.js
@@ -1,12 +1,75 @@
 (function(){
-	var moduleForIndexPage = angular.module('mainModule');
-	moduleForIndexPage.service('indexPageService',['$http',function($http){
+	var moduleForIndexPage = angular.module("mainModule");
+	moduleForIndexPage.service("indexPageService",["$http",function($http){
 		return{
 			getCardColors : _cardColorProvider
 		}
-
-		function _cardColorProvider(){
-			return {'color':'red'}
+		/*############## function declaration ######### */
+		function _cardColorProvider(cardsData){
+			return [{
+			"card_color" : "Blue",
+			"card_identifier" : "1"
+		},
+		{
+			"card_color" : "Brown",
+			"card_identifier" : "2"
+		},
+		{
+			"card_color" : "BurlyWood",
+			"card_identifier" : "3"
+		},
+		{
+			"card_color" : "Chartreuse",
+			"card_identifier" : "4"
+		},
+		{
+			"card_color" : "Cyan",
+			"card_identifier" : "5"
+		},
+		{
+			"card_color" : "DarkGoldenRod",
+			"card_identifier" : "6"
+		},
+		{
+			"card_color" : "DarkKhaki",
+			"card_identifier" : "7"
+		},
+		{
+			"card_color" : "DarkSlateGray",
+			"card_identifier" : "8"
+		},
+		{
+			"card_color" : "Blue",
+			"card_identifier" : "1"
+		},
+		{
+			"card_color" : "Brown",
+			"card_identifier" : "2"
+		},
+		{
+			"card_color" : "BurlyWood",
+			"card_identifier" : "3"
+		},
+		{
+			"card_color" : "Chartreuse",
+			"card_identifier" : "4"
+		},
+		{
+			"card_color" : "Cyan",
+			"card_identifier" : "5"
+		},
+		{
+			"card_color" : "DarkGoldenRod",
+			"card_identifier" : "6"
+		},
+		{
+			"card_color" : "DarkKhaki",
+			"card_identifier" : "7"
+		},
+		{
+			"card_color" : "DarkSlateGray",
+			"card_identifier" : "8"
+		}]
 		}
 	}]);
 })();
