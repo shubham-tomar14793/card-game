967e4acac853cac6606e4021b988d0b76e92475a
shubham <shubham14jul93@gmail.com>
2016-11-25 10:01:21 +0530

    Signed-off-by: shubham <shubham14jul93@gmail.com>added ng-views

--- public/module/mainModule.js
+++ public/module/mainModule.js
@@ -1,3 +1,9 @@
 (function(){
 	var mainModule = angular.module('mainModule',['ngRoute']);
+	mainModule.config(function($routeProvider){
+		$routeProvider.when('/',{
+			templateUrl : 'partial-views/card-game.html'
+		})
+		.otherwise({redirectTo : '/'});
+	});
 })();
